fdbc22c340705f11e22466d60d23d7ae5107358f
siunlucky <faizelfahad2@gmail.com>
2021-06-08 23:39:21 +0700

    Pusying

---
+++ src/overriiding/suarahewan.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package overriiding;
+
+/**
+ *
+ * @author FAIZ
+ */
+public class suarahewan extends hewan{
+    
+    public void suarahewan(){
+        
+        System.out.println("Miaw");
+        
+    }
+    
+    
+
+    
+}
